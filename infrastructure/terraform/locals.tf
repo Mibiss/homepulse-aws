949aaeea6f70e9a872cd7f62e0900d71a7107e70
Mihail Paladi <Mihail.paladi14@gmail.com>
2026-08-09 20:28:54 +0200

    refactor(iam): use AWS managed Lambda logging policy

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -6,13 +6,6 @@ locals {
     Repository  = "homepulse-aws"
   }
 
-  lambda_logging_policy_arn = join("", [
-    "arn:",
-    data.aws_partition.current.partition,
-    ":iam::",
-    data.aws_caller_identity.current.account_id,
-    ":policy/service-role/",
-    "AWSLambdaBasicExecutionRole-2c2367e4-5a30-49bf-95b2-833c79646f77"
-  ])
+  lambda_logging_policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 
 }
